47aab9a587a446eef4fe10288e61a670759f3633
Sarvan-Singh <sarvan1724.becse24@chitkara.edu.in>
2025-11-19 12:46:24 +0530

    Loading Functionality added by materialUi also we write logic for UserPage to get data from firestore

--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -1,8 +1,41 @@
 import React, {useState} from 'react';
+import { db, auth } from '../firebaseConfig';
+import {useEffect} from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import { CircularProgress } from '@mui/material';
 
 const UserPage = () => {
 
-   const[data, setData] =
+   const[data, setData] =useState([]);
+   const[user, loading] = useAuthState(auth);             //to get current logged in user real time
+   const navigate = useNavigate();
+   
+   const fetchUserData = () => {   //USED TO FETCH DATA
+      const resultRef = db.collection('Results');     //REFERENCE given to results collection
+      const { uid } = auth.currentUser;   //getting current logged in user's uid
+      let tempData = [];
+      resultRef.where("userId", "==", uid)
+      .get().then((snapshot) => {            //to fetch all documents from results collection
+        snapshot.docs.forEach((doc) => {
+          tempData.push({...(doc.data())});
+        });
+        setData(tempData);
+      });
+   };
+   
+   useEffect(() => {
+     if(!loading){
+        fetchUserData();     //when compoenent loads then firebase also loads data from firestore
+     }
+     if(!loading && !user){
+      navigate('/');      //if user is not logged in then navigate to home page
+     }
+   },[loading])
+
+     if(loading){
+      return <CircularProgress />;
+     }
 
   return(
     <center>Hello Success</center>
